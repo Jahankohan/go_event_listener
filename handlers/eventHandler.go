d754cb8295f5bd2113a96429368e0e28c0422137
Reza Jahankohan <reza.jahankohan@pixiongames.com>
2023-05-25 04:14:43 +0300

    GetAllEvent Endpoint implemented

---
+++ handlers/eventHandler.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/gin-gonic/gin"
+	"github.com/jahankohan/go_event_listener/model"
+	"github.com/jahankohan/go_event_listener/modules"
+
+	"github.com/jahankohan/go_event_listener/utils"
+)
+
+type EventHandler struct {}
+
+var CollectionCreatedEvents []model.CollectionCreatedEvent
+var TokenMintedEvents []model.TokenMintedEvent
+
+func (eh EventHandler) GetAllEvents(c *gin.Context) {
+	variables := utils.LoadConfig()
+	client, err := ethclient.Dial(variables.AVATestnet.Network)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"error" : "Problem with connecting to Geth",
+			"description" : err.Error(),
+		})
+	}
+	handler := modules.ClientHandler{Client: client, DeployedAddress: variables.AVATestnet.DeployedAddress}
+	allEvents := handler.PullEvents()
+	c.JSON(http.StatusOK, allEvents)
+}
+
